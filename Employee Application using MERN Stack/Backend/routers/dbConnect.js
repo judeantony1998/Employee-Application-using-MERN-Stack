d7179162162716917fdb221c1d02473ad776e0c4
JUDE ANTONY <judeantony1998@gmail.com>
2024-12-02 14:02:00 +0530

    Delete Employee Application using MERN Stack/Backend/routers/dbConnect.js

--- Employee Application using MERN Stack/Backend/routers/dbConnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-mongoose
-  .connect(process.env.mongo_url)
-  .then(() => {
-    console.log("Database is connected");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
